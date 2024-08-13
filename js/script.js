89e1b7588b27f9b742c63220344f91befddb7a6c
Jose Maria <josema.estradaze@gmail.com>
2024-08-13 12:44:50 -0600

    upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 // Ejercicio 1: Suma de números impares del 1 al 100
 let sumaImpares = 0;
-for (let i = 1; i <= 100; i += 2) {
-    sumaImpares += i;
+let i = 1;
+while (i <= 100) {
+    if (i % 2 !== 0) {
+        sumaImpares += i;
+    }
+    i++;
 }
 document.getElementById('resultadoSuma').textContent = `La suma de todos los números impares del 1 al 100 es: ${sumaImpares}`
 
-//Ejercicio 2: Mostrar asteriscos
+// Ejercicio 2: Mostrar asteriscos
 document.getElementById('asteriscosForm').addEventListener('submit', function(event) {
     event.preventDefault();
     const numero = parseInt(document.getElementById('numeroAsteriscos').value);
@@ -16,7 +20,7 @@ document.getElementById('asteriscosForm').addEventListener('submit', function(ev
     document.getElementById('resultadoAsteriscos').textContent = asteriscos;
 })
 
-//Ejercicio 3: Tablas de multiplicar del 1 al 10
+// Ejercicio 3: Tablas de multiplicar del 1 al 10
 let tablas = '';
 for (let i = 1; i <= 10; i++) {
     for (let j = 1; j <= 10; j++) {
